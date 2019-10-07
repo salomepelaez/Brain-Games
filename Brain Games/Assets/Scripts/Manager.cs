3a7c1da6fa7626bc62fe133dcbd09c2d473d8b7b
Salomé Pelaez <spsalome@gmail.com>
2019-10-07 17:19:11 -0500

    Materials created

--- Brain Games/Assets/Scripts/Manager.cs
+++ Brain Games/Assets/Scripts/Manager.cs
@@ -2,13 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rotation : MonoBehaviour
+public class Manager : MonoBehaviour
 {
+    public Material[] materials = new Material[8];
+    public GameObject[] pieces = new GameObject[6];
     private bool show = false;
+    int numTextures = 0;
+
+    private void Start()
+    {
+        for(int i = 0; i < pieces.Length; i++)
+        {
+            GameObject piece = new GameObject();
+            piece.transform.GetChild(numTextures).gameObject.GetComponent<MeshRenderer>();
+            piece.transform.GetChild(numTextures).gameObject.AddComponent<MeshRenderer>().material = materials[Random.Range(0, materials.Length)];
+        }
+
+        numTextures++;
+    }
 
     void Update()
     {
-        if(show == true)
+        if (show == true)
         {
             transform.rotation = Quaternion.Euler(Vector3.Lerp(transform.rotation.eulerAngles, Vector3.up * 180, 2 * Time.deltaTime));
         }
@@ -16,7 +31,7 @@ public class Rotation : MonoBehaviour
         else
         {
             transform.rotation = Quaternion.Euler(Vector3.Lerp(transform.rotation.eulerAngles, Vector3.zero, 2 * Time.deltaTime));
-        }        
+        }
     }
 
     private void OnMouseDown()
@@ -25,7 +40,7 @@ public class Rotation : MonoBehaviour
         Debug.Log("ahhhh");
         Invoke("TurnAround", 3);
     }
-  
+
     private void TurnAround()
     {
         show = false;
