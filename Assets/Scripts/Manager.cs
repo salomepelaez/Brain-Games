0e854d2e2f20696aa78bc91cabb545f374fced02
Salomé Pelaez <spsalome@gmail.com>
2019-10-08 12:57:49 -0500

    Materials added

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -2,15 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Manager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Material[] materials = new Material[8];
+    public GameObject[] pieces = new GameObject[6];
     void Start()
     {
-        
+        GameObject pieza = new GameObject();
+
+        for (int i = 0; i < pieces.Length; i++)
+        {
+            pieza.transform.GetChild(i).GetChild(0).GetComponent<MeshRenderer>().material = materials[Random.Range(0, materials.Length)];
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
         
