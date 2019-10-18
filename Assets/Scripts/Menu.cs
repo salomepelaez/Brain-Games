6f4aa45270ca27ba14773a8b42f22eb5a64169e9
Salomé Pelaez <spsalome@gmail.com>
2019-10-18 15:45:08 -0500

    Fix in bool

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -8,17 +8,25 @@ public class Menu : MonoBehaviour
     public void SinglePlayer()
     {
         SceneManager.LoadScene("OnePlayer");
+
+        if(Manager.lockMouse)
+        {
+            Manager.lockMouse = !Manager.lockMouse;
+        }
     }
 
     public void Multiplayer()
     {
         SceneManager.LoadScene("Multiplayer");
+
+        if (ManagerMP.lockMouse)
+        {
+            ManagerMP.lockMouse = !ManagerMP.lockMouse;
+        }
     }
 
     public void BackToMenu()
     {
         SceneManager.LoadScene("Menu");
-    }
-
-
+    }        
 }
