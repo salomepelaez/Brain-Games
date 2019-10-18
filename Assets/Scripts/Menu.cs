c0a734aa7786ad0030e5685a8cb98d896b81db04
Salomé Pelaez <spsalome@gmail.com>
2019-10-18 13:31:34 -0500

    Menu added

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void SinglePlayer()
+    {
+        SceneManager.LoadScene("OnePlayer");
+    }
+
+    public void Multiplayer()
+    {
+        SceneManager.LoadScene("Multiplayer");
+    }
+
+    public void First()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
